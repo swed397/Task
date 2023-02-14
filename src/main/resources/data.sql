6a899a361ceb0898a3b44e6220a1b599ed785759
Rodion <swed197@mail.ru>
2023-02-14 19:08:07 +0500

    2st part of task

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+insert into movies(name, comment, type, genre, date)
+values ('Терминатор', 'Просто комментарий', 'Полнометражный', 'Боевик', '1984-10-26');
+
+insert into movies(name, comment, type, genre, date)
+values ('Рэмбо', 'Просто комментарий', 'Полнометражный', 'Боевик', '1982-11-10');
+
+insert into movies(name, comment, type, genre, date)
+values ('Трансформеры', 'Просто комментарий', 'Полнометражный', 'Боевик', '2007-06-04');
+
+insert into movies(name, comment, type, genre, date)
+values ('Наваждение', 'Просто комментарий', 'Короткометражный', 'Научный', '1965-01-04');
+
+insert into movies(name, comment, type, genre, date)
+values ('Пила', 'Просто комментарий', 'Короткометражный', 'Ужасы', '2003-07-01');
+
+insert into movies(name, comment, type, genre, date)
+values ('Уэнсдей', 'Просто комментарий', 'Сериал', 'Комедия', '2022-09-22');
+
+insert into movies(name, comment, type, genre, date)
+values ('Тьма', 'Просто комментарий', 'Сериал', 'Ужасы', '2017-09-22');
+
+insert into movies(name, comment, type, genre, date)
+values ('TEST', 'TEST', 'TEST', 'TEST', '2018-10-10');
---
+++ src/test/java/com/example/demo/services/MovieServiceTest.java
@@ -0,0 +1,80 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Movie;
+import com.example.demo.repositories.MovieRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MovieServiceTest {
+
+    @Autowired
+    public MovieService movieService;
+    @MockBean
+    public MovieRepository movieRepository;
+
+    @BeforeEach
+    public void setUp() {
+        Mockito.when(movieRepository.save(Mockito.any(Movie.class)))
+                .thenReturn(new Movie(1L, "Test film",
+                        "TEST", "TEST", "TEST", LocalDate.now()));
+
+        Mockito.when(movieRepository.findAll())
+                .thenReturn(Arrays.asList(new Movie(), new Movie(), new Movie()));
+
+    }
+
+    @Test
+    void saveTest() {
+        Movie movie = new Movie();
+        Movie savedMovie = movieService.save(movie);
+
+        Assertions.assertNotNull(savedMovie);
+        Assertions.assertNotNull(savedMovie.getId());
+    }
+
+    @Test
+    void findAllByNameTest() {
+        ArrayList<Movie> movieArrayList = movieRepository.findAllByNameContainingIgnoreCase("");
+
+        Assertions.assertNotNull(movieArrayList);
+        Assertions.assertEquals(movieArrayList.size(), 0);
+    }
+
+    @Test
+    void findAllByTypeTest() {
+        ArrayList<Movie> movieArrayList = movieRepository.findAllByTypeIgnoreCase("");
+
+        Assertions.assertNotNull(movieArrayList);
+        Assertions.assertEquals(movieArrayList.size(), 0);
+    }
+
+    @Test
+    void findAllByYearTest() {
+        ArrayList<Movie> movieArrayList = movieRepository.findByYear(2018);
+
+        Assertions.assertNotNull(movieArrayList);
+        Assertions.assertEquals(movieArrayList.size(), 0);
+    }
+
+    @Test
+    void findAll() {
+        List<Movie> movieArrayList = movieRepository.findAll();
+
+        Assertions.assertNotNull(movieArrayList);
+        Assertions.assertEquals(movieArrayList.size(), 3);
+    }
+}
