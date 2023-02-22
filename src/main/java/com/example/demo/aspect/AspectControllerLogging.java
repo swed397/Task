3cd17e46e948b506e6c9804a03037de9f63ac86d
Rodion <swed197@mail.ru>
2023-02-22 22:24:19 +0500

    fast hot fix

--- src/main/java/com/example/demo/aspect/AspectControllerLogging.java
+++ src/main/java/com/example/demo/aspect/AspectControllerLogging.java
@@ -4,7 +4,6 @@ import com.example.demo.dtos.MovieCrateDto;
 import com.example.demo.dtos.MovieDto;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
